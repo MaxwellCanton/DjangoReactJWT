011e04ed42f3dbbab44ef74823276cd575e1c322
MaxwellHernandezCanton <devmaxwellcanton@gmail.com>
2025-04-04 03:07:41 -0600

    se agrega action get all y reducer para proyecto

---
+++ frontend/src/redux/actions/proyecto.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import {
+    GET_PROJECT_LIST_SUCCESS,
+    GET_PROJECT_LIST_FAIL
+} from "./types";
+
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = "X-CSRFToken"
+axios.defaults.withCredentials = true;
+
+const client = axios.create({
+    baseURL: process.env.REACT_APP_ENVIRONMENT
+});
+
+export const return_proyectos = () => async dispatch => {
+
+  try {
+    const res = await client.get("/proyectos/api");
+
+    if(res.status === 200){
+      dispatch({type: GET_PROJECT_LIST_SUCCESS, payload: res.data});
+    }else{
+      dispatch({type: GET_PROJECT_LIST_FAIL});
+    }
+
+  } catch (error) {
+    dispatch({type: GET_PROJECT_LIST_FAIL});
+  }
+
+}
---
+++ frontend/src/redux/reducers/proyecto.js
@@ -0,0 +1,26 @@
+import {
+    GET_PROJECT_LIST_SUCCESS,
+    GET_PROJECT_LIST_FAIL,
+} from '../actions/types';
+
+const initialState = {
+    proyectos_list: null,
+};
+
+export default function proyecto(state = initialState, action) {
+    const { type, payload } = action;
+    switch(type) {
+        case GET_PROJECT_LIST_SUCCESS:
+            return {
+                ...state,
+                proyectos_list: payload.proyectos,
+            }
+        case GET_PROJECT_LIST_FAIL:
+            return {
+                ...state,
+                proyectos_list: null,
+            }
+        default:
+            return state
+    }
+}
