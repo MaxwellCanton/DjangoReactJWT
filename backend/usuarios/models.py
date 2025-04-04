9b8f3150109e077d0c585782458af758d47fa40d
MaxwellHernandezCanton <devmaxwellcanton@gmail.com>
2025-04-03 23:07:47 -0600

    TicketUser - cleaning migrations and adding required fields for users

--- backend/usuarios/models.py
+++ backend/usuarios/models.py
@@ -9,6 +9,8 @@ class AppUser(AbstractBaseUser, PermissionsMixin):
 	id = models.AutoField(primary_key=True)
 	email = models.EmailField(max_length=50, unique=True)
 	username = models.CharField(max_length=50)
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = ['username']
 
 	def __str__(self):
 		return self.username
