4b9899a11f217a10f30e6a778aad74d206872a04
MaxwellHernandezCanton <devmaxwellcanton@gmail.com>
2025-04-04 16:12:36 -0600

    se actualiza requirements.txt y se muestra el nombre del cliente y no el id

--- backend/proyectos/serializers.py
+++ backend/proyectos/serializers.py
@@ -4,9 +4,14 @@ from clientes.models import Cliente
 from proyectos.models import Proyecto
 
 class ProyectoSerializer(serializers.ModelSerializer):
+    cliente = serializers.SerializerMethodField()
+
     class Meta:
         model = Proyecto
         fields = '__all__'
+
+    def get_cliente(self, obj):
+        return f"{obj.cliente.nombre}"
 
 class ProyectoPostSerializer(serializers.ModelSerializer):
     cliente = serializers.PrimaryKeyRelatedField(queryset=Cliente.objects.all(), required=False)
