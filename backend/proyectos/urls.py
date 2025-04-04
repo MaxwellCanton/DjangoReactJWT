4f3768eb1bde87d8a65d066b6a3a04ab2ea9d98b
MaxwellHernandezCanton <devmaxwellcanton@gmail.com>
2025-04-04 14:22:23 -0600

    backend para proyectos get all , get by y put

--- backend/proyectos/urls.py
+++ backend/proyectos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from proyectos.views import ProyectoView
+from proyectos.views import ProyectoView, ProyectoByIdView
 
 urlpatterns = [
 
     path('api/',  ProyectoView.as_view()),
+    path('api/<proyecto_id>', ProyectoByIdView.as_view()),
 
 ]
--- backend/proyectos/views.py
+++ backend/proyectos/views.py
@@ -1,6 +1,8 @@
-from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
+from rest_framework.views import APIView
 
 from proyectos.models import Proyecto
 from proyectos.serializers import ProyectoSerializer
@@ -9,21 +11,51 @@ from proyectos.serializers import ProyectoSerializer
 class ProyectoView(generics.GenericAPIView):
 
     serializer_class = ProyectoSerializer
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
 
         if Proyecto.objects.all().exists():
             proyectos = Proyecto.objects.all()
             serializer = ProyectoSerializer(proyectos, many=True)
-            return Response({'proyectos':serializer.data}, status=status.HTTP_200_OK)
+            return Response({'proyectis':serializer.data}, status=status.HTTP_200_OK)
         else:
             return Response({'error':'No hay proyectos'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    def post(self, request):
-        proyecto =  request.data
-        serializer = ProyectoSerializer(data=proyecto)
+    # def post(self, request):
+    #     proyecto =  request.data
+    #     proyecto["usuario"] = request.user.id
+    #     serializer = ProyectoPostSerializer(data=proyecto)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response({"success": True}, status=status.HTTP_200_OK)
+    #     else:
+    #         return Response({"success": serializer.errors})
+
+class ProyectoByIdView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, proyecto_id):
+        if Proyecto.objects.all().exists():
+            proyecto = Proyecto.objects.get(id = proyecto_id)
+            serializer = ProyectoSerializer(proyecto, many=False)
+            return Response({'proyecto':serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error':'No existe proyecto con ese id'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def delete(self, request, proyecto_id):
+        proyecto = get_object_or_404(Proyecto.objects.all(), pk=proyecto_id)
+        proyecto.delete()
+        return Response({"success": True}, status=status.HTTP_200_OK)
+
+    def put(self, request, proyecto_id):
+        data = request.data
+        proyecto = Proyecto.objects.get(id = proyecto_id)
+        serializer = ProyectoSerializer(instance = proyecto, data = data, partial = True)
         if serializer.is_valid():
             serializer.save()
             return Response({"success": True}, status=status.HTTP_200_OK)
         else:
-            return Response({"success": serializer.errors})
+            print(serializer.errors)
+            return Response({"success": False})
