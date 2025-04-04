0300dfef7c4edc7af589df227f4203effa0464a4
MaxwellHernandezCanton <devmaxwellcanton@gmail.com>
2025-04-03 21:47:58 -0600

    initial django project

---
+++ DjangoReactJWT/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for DjangoReactJWT project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoReactJWT.settings')
+
+application = get_wsgi_application()
