eb2d50cc6779f62226d08414c0deed0448d98d21
MaxwellHernandezCanton <devmaxwellcanton@gmail.com>
2025-04-03 22:59:39 -0600

    TicketUser - model for users addded

--- backend/Usuarios/models.py
+++ backend/Usuarios/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 # Create your models here.
+
+
+class AppUser(AbstractBaseUser, PermissionsMixin):
+	id = models.AutoField(primary_key=True)
+	email = models.EmailField(max_length=50, unique=True)
+	username = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.username
