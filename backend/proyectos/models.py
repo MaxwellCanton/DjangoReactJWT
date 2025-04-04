3fc433dc0b900caec4bbb688e1142910aa4bfe1e
MaxwellHernandezCanton <devmaxwellcanton@gmail.com>
2025-04-04 01:32:59 -0600

    TicketProyectos - Modelo para proyectos

--- backend/proyectos/models.py
+++ backend/proyectos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from backend.clientes.models import Cliente
+
+
+class Proyecto(models.Model):
+    ESTADOS = [
+        ('pendiente', 'Pendiente'),
+        ('en_progreso', 'En Progreso'),
+        ('completado', 'Completado'),
+    ]
+
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name="proyectos")
+    nombre = models.CharField(max_length=255)
+    descripcion = models.TextField(blank=True, null=True)
+    estado = models.CharField(max_length=20, choices=ESTADOS, default='pendiente')
+    creado_en = models.DateTimeField(auto_now_add=True)
