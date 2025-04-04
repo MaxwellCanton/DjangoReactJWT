f6d2441dee071ad135d8211ff8cdc0507de0729b
MaxwellHernandezCanton <devmaxwellcanton@gmail.com>
2025-04-04 01:32:09 -0600

    TicketCliente - Modelo para cliente

--- backend/clientes/models.py
+++ backend/clientes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from backend.usuarios.models import AppUser
+
+
+class Cliente(models.Model):
+    usuario = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    telefono = models.CharField(max_length=20, blank=True, null=True)
+    creado_en = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
